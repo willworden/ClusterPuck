3699267e3ecc594316769d3268060dd92728729a
Will Worden, Esq <will.worden@visioncritical.com>
2014-08-21 10:34:43 -0700

    [ww] first commit

---
+++ flare3.js
@@ -0,0 +1,73 @@
+{
+  "name": "flare",
+  "children": [
+    {
+      "name": "bunch of wills",
+      "children": [
+        {
+          "name": "city",
+          "children": [
+            { "name": "Vancouver", "size": 137 },
+            { "name": "Calgary", "size": 11 },
+            { "name": "Montreal", "size": 2 }
+          ]
+        },
+        {
+          "name": "gender",
+          "children": [
+            { "name": "Male", "size": 143 },
+            { "name": "Female", "size": 7 }
+          ]
+        },
+        {
+          "name": "Income",
+          "children": [
+            { "name": "10K", "size": 139 },
+            { "name": "50K", "size": 11 }
+          ]
+        },
+        {
+          "name": "MaritalStatus",
+          "children": [
+            { "name": "Single", "size": 139 },
+            { "name": "Married", "size": 5 },
+            { "name": "Divorced", "size": 6 },
+            { "name": "Widowed", "size": 4 }
+          ]
+        }
+      ]
+    },
+
+    {
+      "name": "cluster2",
+      "children": [
+        {
+          "name": "FavoriteFood",
+          "children": [
+            { "name": "Kebab", "size": 1230 },
+            { "name": "Shawarma", "size": 610 },
+            { "name": "Turkish Salad", "size": 2 }
+          ]
+        },
+        {
+          "name": "Education",
+          "children": [
+            { "name": "Highschool", "size": 600 },
+            { "name": "College", "size": 409 },
+            { "name": "University", "size": 79 },
+            { "name": "Postgrad", "size": 107 }
+          ]
+        },
+        {
+          "name": "Age",
+          "children": [
+            { "name": "below 25", "size": 46 },
+            { "name": "26 to 35", "size": 191 },
+            { "name": "36 to 45", "size": 84 },
+            { "name": "46 to 55", "size": 8 }
+          ]
+        }
+      ]
+    }    
+  ]
+}
